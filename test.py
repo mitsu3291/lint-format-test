e9cfa9e2410a0fc5c3c4e510352a5e39db93f47e
morita-80 <mitsuki.morita@80and.co>
2023-07-07 15:53:25 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import numpy 
+
+import csv
+
+def add(a, b):
+    return a+b
+
+if __name__ == "__main__":
+    print("hi")
+
+    num1 = 1
+    num2 = 2
+    
+    added_num = add(num1, num2)
+
+    print(added_num)
